face70f561acd90b05e7938509cfb9a0d3965522
Supriyo Ain <ainsupriyo@gmail.com>
2024-03-07 09:10:39 +0530

    Data Validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
 
 
@@ -7,6 +8,17 @@ try:
     logger.info(f"------ Stage {STAGE_NAME} has started ------")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f"------ Stage {STAGE_NAME} has completed ------\n\n<=============>")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f"------ Stage {STAGE_NAME} has started ------")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f"------ Stage {STAGE_NAME} has completed ------\n\n<=============>")
 except Exception as e:
     logger.exception(e)
--- src/textSummarizer/pipeline/stage_01_data_ingestion.py
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -10,6 +10,6 @@ class DataIngestionTrainingPipeline:
     def main(self):
         config = ConfigurationManager()
         data_ingestion_config = config.get_data_ingestion_config()
-        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion = DataIngestion(config = data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
